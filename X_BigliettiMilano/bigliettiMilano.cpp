968148218f3b4fb491a615c6afb39e0525f7355a
sweetcri <106494926+sweetcri@users.noreply.github.com>
2024-05-10 07:04:43 +0000

    biglietti milano

---
+++ X_BigliettiMilano/bigliettiMilano.cpp
@@ -0,0 +1,59 @@
+/********************************************************************************
+Dolce Davide
+3CIN
+04/05/2024
+*******************************************************************************/
+#include <stdio.h>
+#include <assert.h>
+
+using namespace std;
+
+int compra(int N, int M, int A, int B) {
+  if (N < 0){
+      return 0;
+  }
+  
+  if (M < 0){
+     return N * A; 
+  } 
+  
+  if (A < 0 || B < 0){
+    if (A < B){
+        return A * N;
+    } else {
+        return B * M;
+    }
+}
+  
+  if (A * N < B){
+      return A * N;
+      
+  } 
+
+  int tmp = N / M;
+
+  int carnet = tmp * B;
+
+  if (N % M * A < B) return carnet + A * (N % M);
+
+  return carnet + B;
+
+}
+
+int main() {
+    FILE *fr, *fw;
+    int N, M, A, B;
+
+
+    fr = fopen("input.txt", "r");
+    fw = fopen("output.txt", "w");
+    
+    assert(4 == fscanf(fr, "%d%d%d%d", &N, &M, &A, &B));
+
+    fprintf(fw, "%d\n", compra(N, M, A, B));
+    fclose(fr);
+    fclose(fw);
+
+    return 0;
+
+}
